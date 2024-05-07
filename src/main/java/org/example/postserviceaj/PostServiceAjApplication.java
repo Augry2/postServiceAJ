d7fb952030cbf75aba8796ef9a70b5e2c2290ba8
jnnilsson <jn.jensnilsson@gmail.com>
2024-05-07 11:31:38 +0200

    add mongodb connection, saving to db
    Co-authored-by: August Rydnell <august.rydnell@gmail.com>

--- src/main/java/org/example/postserviceaj/PostServiceAjApplication.java
+++ src/main/java/org/example/postserviceaj/PostServiceAjApplication.java
@@ -2,8 +2,10 @@ package org.example.postserviceaj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class PostServiceAjApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/example/postserviceaj/repository/MessageRepository.java
+++ src/main/java/org/example/postserviceaj/repository/MessageRepository.java
@@ -1,7 +1,30 @@
 package org.example.postserviceaj.repository;
 
 import org.example.postserviceaj.entity.MessageEntity;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface MessageRepository extends ListCrudRepository<MessageEntity, String>{
+@Repository
+public interface MessageRepository extends MongoRepository<MessageEntity, String> {
+//
+//    Page<MessageEntity> findByFromEqualsIgnoreCaseAndToEqualsIgnoreCase(String fromUser,
+//                                                                        String toUser,
+//                                                                        Pageable pageable);
+//
+//    @Query("""
+//            {
+//
+//            "from": { "$in":[ ?0, ?1 ] },
+//            "to": { "$in": [ ?1, ?0]}
+//
+//            }
+//            """
+//    )
+//    Page<MessageEntity> findMessages(@Param("from") String fromUser, @Param("to") String toUser, Pageable pageable);
+
 }
+
