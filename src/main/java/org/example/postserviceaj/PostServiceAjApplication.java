fc69cfb37dcaea035876fd5182a91e1305f9c101
jnnilsson <jn.jensnilsson@gmail.com>
2024-05-07 09:17:58 +0200

    initial commit
    Co-authored-by: August Rydnell <august.rydnell@gmail.com>

---
+++ src/main/java/org/example/postserviceaj/PostServiceAjApplication.java
@@ -0,0 +1,13 @@
+package org.example.postserviceaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceAjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostServiceAjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/postserviceaj/PostServiceAjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.postserviceaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceAjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
